606b9cbbffb4a0ec53ba1cb954cc2e8041b8dba3
neko113 <alstn113@gmail.com>
2022-03-09 15:54:00 +0900

    course 14

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ npm init svelte@next <앱 이름>
 Skeleton-project -> No -> No -> No
 yarn // 모듈 설치
 yarn dev // 시작
+
+$lib 이런식으로 src alias 가능
+
+\_\_layout.svelte는
---
+++ src/routes/about.svelte
@@ -0,0 +1,15 @@
+
+<div class="about">
+  <h2>About us</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos et saepe nisi magnam expedita quaerat praesentium, at rerum, nulla dignissimos illum totam aut in qui ipsam repellat, animi cupiditate repudiandae?</p>
+  <a href="/guides">View Guides</a>
+  <a href="/">Home</a>
+</div>
+
+<style>
+  .about {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
